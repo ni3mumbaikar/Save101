4d12d707a0cee58d9010c5ee5fc79641e5c48e61
ni3mumbaikar <nitikale035@gmail.com>
2023-03-09 01:25:58 +0530

    remove Auth0 support

--- app/src/main/java/in/ni3mumbaikar/save101/MainActivity.kt
+++ app/src/main/java/in/ni3mumbaikar/save101/MainActivity.kt
@@ -10,14 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.auth0.android.Auth0
 import `in`.ni3mumbaikar.save101.ui.theme.Save101Theme
-import `in`.ni3mumbaikar.save101.util.AuthUtil
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AuthUtil.account = Auth0(getString(R.string.com_autho_client_id),getString(R.string.com_auth0_domain))
         setContent {
             Save101Theme {
                 // A surface container using the 'background' color from the theme
@@ -29,7 +26,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        AuthUtil.initAuth(this)
     }
 }
 
